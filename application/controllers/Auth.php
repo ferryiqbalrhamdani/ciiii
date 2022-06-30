a473ab77062e08c94ceb1a33a7a90a914d630f2d
ferryiqbalrhamdani <rhamdani128@gmail.com>
2022-06-30 18:14:47 +0700

    update 2

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,6 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller {
     public function index() {
+        $this->load->view('templates/auth_heder');
         $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
     }
 }
